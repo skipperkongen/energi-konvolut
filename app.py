37cf88bc9bb2338e317686f44888f25513800d3a
Pimin Konstantin Kefaloukos <skipperkongen@gmail.com>
2022-11-10 20:55:01 +0100

    start

---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import pydeck as pdk
+
+st.title('Dæk Danmarks energibehov')
+
+with st.sidebar:
+    solpaneler_km2 = st.slider('Solpaneler km2', 0, 200, 0)  # min: 0h, max: 23h, default: 17h
+    vindmøller_km2 = st.slider('Vindmøller km2', 0, 200, 0)  # min: 0h, max: 23h, default: 17h
+    batterier_km2 = st.slider('Batterier km2', 0, 100, 0)  # min: 0h, max: 23h, default: 17h
+
+df = pd.DataFrame(
+   np.random.randn(1000, 2) / [50, 50] + [55.76, 12.4],
+   columns=['lat', 'lon'])
+
+st.pydeck_chart(pdk.Deck(
+    map_style=None,
+    initial_view_state=pdk.ViewState(
+        latitude=55.76,
+        longitude=11.8,
+        zoom=6,
+        pitch=50,
+    ),
+    layers=[
+        pdk.Layer(
+           'HexagonLayer',
+           data=df,
+           get_position='[lon, lat]',
+           radius=2000,
+           elevation_scale=4,
+           elevation_range=[0, 1000],
+           pickable=True,
+           extruded=True,
+        ),
+        pdk.Layer(
+            'ScatterplotLayer',
+            data=df,
+            get_position='[lon, lat]',
+            get_color='[200, 30, 0, 160]',
+            get_radius=200,
+        ),
+    ],
+))
